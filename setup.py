f1cd0195074dca11e84e9e55a29fa52ea0dcd92b
sebastiangarciaacosta <sebastian.garcia@wearemo.com>
2022-04-26 18:37:30 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Performing loan approval with shallow machine learning models',
+    author='ICESI University',
+    license='',
+)
